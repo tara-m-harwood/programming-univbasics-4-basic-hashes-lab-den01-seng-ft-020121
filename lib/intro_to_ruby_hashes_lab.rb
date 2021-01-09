58f92f2996aea79dd95f9d77bdab7138b8fdff4d
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 16:36:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,16 +4,16 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my_hash = [:best => "me"]
+ {:best => "me"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  my_hash = [:name => "Grace Hopper"]
+ {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  my_hash = [:id => number]
+  {:id => number}
 end
